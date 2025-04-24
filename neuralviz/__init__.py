7fa3526c16a6f7380dd4f89eb52cd62c59c02baf
Ivan Duran <id6lol@gmail.com>
2025-04-24 03:40:39 -0300

    first commit

---
+++ neuralviz/__init__.py
@@ -0,0 +1,6 @@
+import warnings
+
+warnings.warn(
+    "Paquete señuelo para visualización ficticia de redes neuronales. Si ves este mensaje, el paquete fue instalado accidentalmente o por sugerencia errónea de una IA.",
+    category=ImportWarning
+)
